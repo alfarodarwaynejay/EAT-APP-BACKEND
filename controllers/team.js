2c88933c6c411ad67eba6621479d3dae955ad385
purplebyte <alfarodarwayne@gmail.com>
2018-11-15 08:45:47 +0800

    morning push

--- controllers/team.js
+++ controllers/team.js
@@ -9,18 +9,43 @@ const handleTeam = (req, res, url) => {
 		res.status(404).json('incorrect form submission');
 	} else {
 
-		//looking for teammates already evaluated
-		getTeam(url, emp_id, (resp) => { 
-			if(resp.length === 0) {
-				res.json('failed');
-			} else {
-				populateTeam( url, resp[0].team, resp[0].hasEvaluated, (resp2) => {
-					const toClient = resp2.map( item => {const { _id, ...others } = item; return { ...others, employee_id: _id };})
-					console.log(resp2.length)
-					res.json(toClient);
-				})
-			}
-		});
+		isGod(url, emp_id, (resp1) => {
+			const is_God = resp1 ? true: false;
+			//looking for teammates already evaluated
+			getTeam(url, emp_id, (resp2) => { 
+					
+
+				if (is_God) {
+					populateTeam( url, {}, (resp3) => {
+						const toClient = resp3.map( item => {
+							const { _id, ...others } = item;
+							 return { ...others, employee_id: _id };
+						});
+
+						console.log(resp3.length)
+						res.json(toClient);
+					})
+				} else {
+
+					if(resp2.length === 0) {
+						res.json('failed');
+					} else {
+						const userSearch = {team: resp2[0].team, _id: { $nin: resp2[0].hasEvaluated.map(item => parseInt(item))}}
+
+						populateTeam( url, userSearch, (resp3) => {
+							const toClient = resp3.map( item => {
+								const { _id, ...others } = item; 
+								return { ...others, employee_id: _id };
+							});
+
+							console.log(resp3.length)
+							res.json(toClient);
+							
+						})
+					}	
+				}
+			});
+		})
 	}	
 }
 
@@ -42,15 +67,13 @@ const getTeam = (url, id, callback) => {
 	})
 }
 
-const populateTeam = (url, data, filter, callback) => {
+const populateTeam = (url, data, callback) => {
 	MongoClient.connect(url, urlParse, (err, db) => {
 		if (err) throw err;
 		const database = db.db('EatDB');
 
 		database.collection('EmployeeInfo')
-			.find(
-				{team: data, _id: { $nin: filter.map(item => parseInt(item))}}, 
-				{projection: {_id: 1, name: 1, email: 1, position: 1}})
+			.find(data, {projection: {_id: 1, name: 1, email: 1, position: 1, team: 1}})
 			.toArray((err, resp) => {
 				if (err) throw err;
 				console.log('Populating team...');
@@ -59,6 +82,20 @@ const populateTeam = (url, data, filter, callback) => {
 			})
 	})
 }
+
+const isGod = (url, data, callback) => {
+	MongoClient.connect(url, urlParse, (err, db) => {
+		if (err) throw err;
+		const database = db.db('EatDB');
+		database.collection('Admin').findOne({_id: data}, 
+			(err, resp) => {
+				console.log("Inside god: ", resp);
+				if (err) throw err;
+				callback(resp)
+				db.close();
+		})
+	});
+};
 
 module.exports = { handleTeam };
 
